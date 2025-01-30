a59ea36ed8727d85563ea9a247d571e49f9aac6a
khoanguyen-dev1 <kn949892@gmail.com>
2025-01-30 22:14:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-from flask import Flask, request, jsonify
+import logging
+from flask import Flask, request, jsonify, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_cors import CORS
+import os
 import requests
-from bs4 import BeautifulSoup
-import json
+import time
+import re
+import random
 
 app = Flask(__name__)
+CORS(app)
+key_regex = r'let content = \("([^"]+)"\);'
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
+port = int(os.getenv('PORT', 8080))
+
+# Cấu hình logging
+logger = logging.getLogger('api_usage')
+logger.setLevel(logging.INFO)
+
+log_file_path = '/tmp/api_usage.log'
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+def get_client_ip():
+    """Hàm để lấy địa chỉ IP của client, xem xét cả trường hợp đằng sau proxy."""
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0].split(',')[0].strip()
+    else:
+        ip = request.remote_addr
+    return ip
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/socialwolvez', methods=['GET'])
 def socialwolvez():
@@ -38,3 +71,9 @@ def socialwolvez():
 
     except requests.RequestException as e:
         return jsonify({'error': 'Failed to make request to the provided URL.', 'details': str(e)}), 500
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False  # Đảm bảo rằng debug=False trong môi trường sản xuất
+    )
