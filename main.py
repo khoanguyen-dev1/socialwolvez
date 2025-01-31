70367b2a6aa954d99c25d8d6b9134bec2d244af2
khoanguyen-dev1 <kn949892@gmail.com>
2025-01-31 22:26:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/socialwolvez', methods=['GET'])
 def socialwolvez():
